80e37b78a902c47b944ae0a294e60a8b0fb92e35
caasinehc <31968674+caasinehc@users.noreply.github.com>
2020-08-10 16:33:41 -0400

    I am well aware that this is not going to work

--- index.js
+++ index.js
@@ -17,6 +17,38 @@ const TOKEN = process.env.TOKEN
 // Login the bot
 bot.login(TOKEN);
 
+// Functions
+function getUUID(username) {
+	// Return a promise that will resolve to the UUID
+	return new Promise((resolve, reject) => {
+		// HTTP request options
+		const OPTIONS = {
+			host: "api.mojang.com",
+			path: "/users/profiles/minecraft/" + username
+		};
+		
+		// Callback for when we recieve data from the HTTP request
+		function callback(response) {
+			// Start with an empty string as the response
+			let responseStr = "";
+			
+			// When we recieve a chunk of data, append it to the end of the reponse string
+			response.on("data", chunk => {
+				responseStr += chunk;
+			});
+			
+			// When we have recieved the full response, resolve the promise and return it
+			response.on("end", () => {
+				const resp = JSON.parse(responseStr);
+				resolve(WATSON_URL);
+			});
+		}
+		
+		// Send out the HTTP request
+		HTTP.request(OPTIONS, callback).end();
+	});
+}
+
 // When the bot is ready, log to the console
 bot.on("ready", () => {
 	console.info(`Logged in as ${bot.user.tag}!`);
@@ -33,24 +65,15 @@ bot.on("message", msg => {
 	
 	if(cmdArgs[0].toLowerCase() === "!sb") {
 		
-		// Command 2
-		if(cmdArgs[1].toLowerCase() === "hi") {
-			msg.channel.send("imagine talking to a discord bot");
-		}
-		
-		// Command 3
-		else if(cmdArgs[1].toLowerCase() === "get on") {
-			msg.channel.send("get in the fucking call right now");
-		}
-		
-		// WIP
-		else if(cmdArgs[1].toLowerCase() === "double") {
-			msg.channel.send("Double Trouble Triple Trouble");
-		}
-		
 		// UUID Retrieval
 		else if(cmdArgs[1].toLowerCase() === "uuid") {
-			msg.channel.send("You are asking about the player named " + cmdArgs[2]);
+			const username = cmdArgs[2];
+			
+			getUUID(username).then(UUID => {
+				msg.channel.send(`The UUID of player "${username}" is "${UUID}"`);
+			});
+			
 		}
+		
 	}
 });
